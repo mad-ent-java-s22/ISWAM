a2fc6bbbebc44b5fea0851298788208a71035ea9
David Calabrese <david.thomas.calabrese@gmail.com>
2022-01-27 12:44:36 -0600

    add test servlet

---
+++ src/main/java/org/davidcalabrese/controller/ExampleServlet.java
@@ -0,0 +1,20 @@
+package org.davidcalabrese.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(
+        urlPatterns = {"/example_servlet"}
+)
+public class ExampleServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.println("Hi from the servlet");
+    }
+}
